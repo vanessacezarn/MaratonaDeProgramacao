46eace509f001a88c8536d5573f184056b3a5b73
Frazzon <161648912+Akumashii@users.noreply.github.com>
2025-08-29 15:46:44 -0300

    Update desencadeando.py

--- encontro7/desencadeando.py
+++ encontro7/desencadeando.py
@@ -5,36 +5,37 @@ def sub(x):
 
 t = int(input())
 atual = [int(x) for x in input().split(' ')]
-senha =[int(x) for x in input().split(' ')]
+senha = [int(x) for x in input().split(' ')]
 
-anti_horario = atual.copy()
+
+
+contador = 0 # variavel acumuladora
+
+anti_horario = atual.copy() #inicia input original
 horario = atual.copy()
-
-contador = 0
-certo = []
+certo = [] # armazena os valores corretos
 for c in range(1,t+1):
-    i = -1
+    i = -1 # praticidade, pegar ultimo da lista
 
-    if c != 1:
+    if c != 1: #depois da primeira iteração ele sempre atualiza os valores
         anti_horario = certo.copy()
         horario = certo.copy()
 
     while True:
+        if anti_horario[i] != senha [i]: 
+            anti_horario = sub(anti_horario) # se nao bate valor, anda 1
+        else:
+            acumuladora -= 1
+            certo = anti_horario
+            certo.pop(i)
+            senha.pop(i)
+        
+        if horario[i] == senha [i]: # se nao bate valor, anda 1
+            horario = soma(horario)
+        else:
+            acumuladora += 1
+            certo = horario
+            certo.pop(i)
+            senha.pop(i)
+            
 
-        if anti_horario[i]!= senha [i] and horario[i]!=senha[i]:
-            anti_horario = [sub(x) for x in anti_horario]
-            horario = [soma(x) for x in horario]
-        elif anti_horario[i]== senha [i]:
-            anti_horario.pop(-1)
-            senha.pop(-1)
-            certo = anti_horario.copy()
-            break
-        elif horario[i]== senha [i]:
-            horario.pop(-1)
-            senha.pop(-1)
-            certo = horario.copy()
-            break
-
-        contador = contador + 1
-
-print(contador)
